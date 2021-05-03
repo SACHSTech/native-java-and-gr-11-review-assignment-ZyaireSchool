39a931f76f8d6d364b5289c0c1bebde85bcc7e95
Ziming Qu <>
2021-05-03 02:43:51 +0000

    part3

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -1,7 +1,22 @@
 package gr11review.part1;
 import java.io.*;
-public class Review2{
+
+public class Review3{
   public static void main(String[] args) throws IOException{
+
+    int count;
+
+    for(count = 20; count < 100; count++){
+      if(count % 2 > 0){
+        System.out.println(count);
+      }
+    }
     
+    System.out.println("                                                                       ");
+    
+    for (count = 29; count >= 2; count--){
+        System.out.println(count);
+    }
+
   }
 }
--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,6 +1,6 @@
 package gr11review.part1;
 import java.io.*;
-public class Review2{
+public class Review4{
   public static void main(String[] args) throws IOException{
     BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
   }
